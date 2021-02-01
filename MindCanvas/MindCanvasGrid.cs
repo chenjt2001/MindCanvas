784b60e624a93a0cfb03e0791d24aaddb1a06400
chenjt2001 <chenjintao2001@gmail.com>
2021-02-01 12:52:42 +0800

    temp

---
+++ MindCanvas/MindCanvasGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml.Controls;
+
+namespace MindCanvas
+{
+    public class MindCanvasGrid: Grid
+    {
+        public MindMapCanvas mindMapCanvas;
+        public InkCanvas inkCanvas;
+
+        public MindCanvasGrid(MindMapCanvas mindMapCanvas, InkCanvas inkCanvas)
+        {
+            this.mindMapCanvas = mindMapCanvas;
+            this.inkCanvas = inkCanvas;
+        }
+    }
+}
